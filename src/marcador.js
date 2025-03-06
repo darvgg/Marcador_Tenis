4785c410510becfc0797a60b810c23fe71a0d668
Diego Rios <diegoarv08@hotmail.com>
2025-03-06 13:16:36 -0400

    Cambios en el HTML

---
+++ src/marcador.js
@@ -0,0 +1,8 @@
+function mostrar_resultado (score_1,score_2){
+    let resultado="";
+    if (score_1==0 && score_2==0)
+            resultado = "Love - Love";
+    return resultado;
+}
+
+export {mostrar_resultado};
