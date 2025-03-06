c2f8a46344d9dbc2b49b2442d40fcc43ea075782
Diego Rios <diegoarv08@hotmail.com>
2025-03-06 13:33:23 -0400

    Caso 2 15-0

--- src/marcador.js
+++ src/marcador.js
@@ -1,7 +1,11 @@
 function mostrar_resultado (score_1,score_2){
     let resultado="-";
-    if (score_1==0 && score_2==0)
-            resultado = "Love - Love";
+    if (score_1==0 && score_2==0){
+        resultado = "Love - Love";
+    }
+    else if (score_1==15 && score_2 == 0){
+        resultado = "15 - 0";
+    }
     return resultado;
 }
 
--- src/marcador.test.js
+++ src/marcador.test.js
@@ -1,7 +1,12 @@
 import {mostrar_resultado} from "./marcador.js";
 
-describe("Marcador iguales a 0", () => {
+describe("Caso 1: Marcador iguales a 0", () => {
   it("deberia mostrar Love - Love", () => {
     expect(mostrar_resultado(0,0)).toEqual("Love - Love");
+  });
+});
+describe("Caso 2: Marcador 15 a 0", () => {
+  it("deberia mostrar 15 - 0", () => {
+    expect(mostrar_resultado(15,0)).toEqual("15 - 0");
   });
 });
