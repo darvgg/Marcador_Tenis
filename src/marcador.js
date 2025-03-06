a916dd01b2b98384c1c79f6fde8091f9abe6f8ff
Diego Rios <diegoarv08@hotmail.com>
2025-03-06 13:52:24 -0400

    Caso 5 0-15

--- src/marcador.js
+++ src/marcador.js
@@ -1,17 +1,14 @@
+
+const puntuaciones = ["0", "15", "30", "40"];
 function mostrar_resultado (score_1,score_2){
     let resultado="-";
     if (score_1==0 && score_2==0){
         resultado = "Love - Love";
     }
-    else if (score_1==15 && score_2 == 0){
-        resultado = "15 - 0";
+    else if (score_1 < 4 && score_2 < 4){
+        resultado = puntuaciones[score_1] + " - " +puntuaciones [score_2];
     }
-    else if (score_1==30 && score_2 == 0){
-        resultado = "30 - 0";
-    }
-    else if (score_1==40 && score_2 == 0){
-        resultado = "40 - 0";
-    }
+
     return resultado;
 }
 
--- src/marcador.test.js
+++ src/marcador.test.js
@@ -7,17 +7,23 @@ describe("Caso 1: Marcador iguales a 0", () => {
 });
 describe("Caso 2: Marcador 15 a 0", () => {
   it("deberia mostrar 15 - 0", () => {
-    expect(mostrar_resultado(15,0)).toEqual("15 - 0");
+    expect(mostrar_resultado(1,0)).toEqual("15 - 0");
   });
 });
 describe("Caso 3: Marcador 30 a 0", () => {
   it("deberia mostrar 30 - 0", () => {
-    expect(mostrar_resultado(30,0)).toEqual("30 - 0");
+    expect(mostrar_resultado(2,0)).toEqual("30 - 0");
   });
 });
 
 describe("Caso 4: Marcador 40 a 0", () => {
   it("deberia mostrar 40 - 0", () => {
-    expect(mostrar_resultado(40,0)).toEqual("40 - 0");
+    expect(mostrar_resultado(3,0)).toEqual("40 - 0");
+  });
+});
+
+describe("Caso 5: Marcador 0 a 15", () => {
+  it("deberia mostrar 0 - 15", () => {
+    expect(mostrar_resultado(0,1)).toEqual("0 - 15");
   });
 });
